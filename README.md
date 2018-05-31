0bcf466bb90a7548c8ab64fc8da761ad677fef1e
wendy <wendy@youlinai.com>
2018-05-30 20:03:34 -0400

    before chapter 5

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DjangoDemo
 A polls application following django document getting started
+
+# Environment
+
+`python` at least `2.7.5`
+`Django` using `1.11.3`
---
+++ mysite/polls/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from polls.models import Question, Choice
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(
+            request,'polls/detail.html',{
+                'question':question,
+                'error_message':"You didn't select a choice"
+            }
+        )
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        #always use the redirect after post data
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
